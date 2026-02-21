90e6ae9a97b4ab68974f48a71a9685e942fec909
Eridhonson Oliveira <eridhonson@outlook.com>
2026-02-20 21:40:20 -0500

    Criação da pasta script; separação dos sqls em arquivos que identifiquem suas funcionalidades

---
+++ corridas/scripts/create_table_corridas.sql
@@ -0,0 +1,3 @@
+CREATE TABLE corridas(data DATE PRIMARY KEY, distancia INT, tempo TIME);
+
+CREATE TABLE corridas_strava(data DATE PRIMARY KEY, distancia_strava INT, tempo_strava TIME);
---
+++ corridas/scripts/insert_values_corridas_strava.sql
@@ -0,0 +1,5 @@
+-- CREATE TABLE corridas_strava(data DATE PRIMARY KEY, distancia_strava INT, tempo_strava TIME);
+
+INSERT INTO corridas_strava VALUES('2026-02-20','8410','01:17');
+
+SELECT * FROM corridas_strava;
