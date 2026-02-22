964a1802ecfa669ec59294720565da2bb448995a
Eridhonson Oliveira <eridhonson@outlook.com>
2026-02-22 13:25:25 -0500

    restaurando arquivos

---
+++ corridas/scripts/delete-corrida.sql
@@ -0,0 +1,5 @@
+SELECT * FROM corridas;
+
+-- Removendo valor do banco de dados
+DELETE FROM corridas
+WHERE distancia='0';
---
+++ corridas/scripts/insert_values_corridas.sql
@@ -0,0 +1,7 @@
+INSERT INTO corridas VALUES('2026-02-16','7330','01:29');
+INSERT INTO corridas VALUES('2026-02-17','7390','01:35');
+INSERT INTO corridas VALUES('2026-02-18','7380','01:28');
+INSERT INTO corridas VALUES('2026-02-20','8750','01:23');
+
+-- Inserindo um valor errado no banco de dados
+-- INSERT INTO corridas VALUES('2026-02-19','0000','00:00');
