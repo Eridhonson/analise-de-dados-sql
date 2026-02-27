696369b963b1450b294fe553dbb22793a29812ff
Eridhonson Oliveira <eridhonson@outlook.com>
2026-02-27 18:06:28 -0500

    dados do dia 27-02-2026 adicionados

--- corridas/scripts/insert_values_corridas.sql
+++ corridas/scripts/insert_values_corridas.sql
@@ -3,6 +3,7 @@ INSERT INTO corridas VALUES('2026-02-17','7390','01:35');
 INSERT INTO corridas VALUES('2026-02-18','7380','01:28');
 INSERT INTO corridas VALUES('2026-02-20','8750','01:23');
 INSERT INTO corridas VALUES('2026-02-23','8570','01:33');
+INSERT INTO corridas VALUES('2026-02-27','3850','00:49');
 
 -- Inserindo um valor errado no banco de dados
 -- INSERT INTO corridas VALUES('2026-02-19','0000','00:00');
